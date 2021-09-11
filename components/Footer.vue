180f92913f5b862d34399f27b41f70d9ddf52ffd
snretuerma <snretuerma@up.edu.ph>
2021-09-11 21:56:35 +0800

    Commit icf-frontend app

---
+++ components/Footer.vue
@@ -0,0 +1,164 @@
+<template>
+  <v-footer min-width="100%" padless absolute color="grey darken-3">
+    <v-container>
+        <v-card 
+            flat tile class="transparent white--text text-center"
+        >
+            <v-card-text flat class="white--text text-left" >
+                <v-row>
+                    <v-col>
+                        <v-card flat color="transparent" class="white--text" min-width="500px">
+                            <v-row>
+                                <v-col justify="center" align="center" class="mt-5">
+                                    <v-img
+                                    contain
+                                    lazy-src="/images/logo_full-white.png"
+                                    max-height="85"
+                                    max-width="120"
+                                    src="/images/logo_full-white.png"
+                                ></v-img>
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-col>
+                    <v-spacer></v-spacer>
+                    <v-col>
+                        <v-card flat color="transparent" class="white--text" min-width="300px">
+                            <v-row>
+                                <v-col>
+                                    <p>
+                                        Company
+                                    </p>
+                                    <p>
+                                        What We Do
+                                    </p>
+                                    <p>
+                                        Investors
+                                    </p>
+                                    <p>
+                                        Resources
+                                    </p>
+                                </v-col>
+                                <v-col>
+                                    <p>
+                                        Blog
+                                    </p>
+                                    <p>
+                                        News & Events
+                                    </p>
+                                    <p>
+                                        Careers
+                                    </p>
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-col>
+                    <v-spacer></v-spacer>
+                        <v-col class="pt-0">
+                            <v-card flat color="transparent" class="white--text" min-width="400px">
+                                <v-row>
+                                    <v-col>
+                                        <v-list dense color="transparent">
+                                            <v-list-item>
+                                                <v-list-item-icon>
+                                                    <v-icon color="white">
+                                                        mdi-email
+                                                    </v-icon>
+                                                </v-list-item-icon>
+                                                <v-list-item-content>
+                                                <v-list-item-title class="white--text">
+                                                        contact@adec-innovations.com
+                                                    </v-list-item-title>
+                                                </v-list-item-content>
+                                            </v-list-item>
+
+                                            <v-list-item>
+                                                <v-list-item-icon>
+                                                    <v-icon color="white">
+                                                        mdi-phone
+                                                    </v-icon>
+                                                </v-list-item-icon>
+                                                <v-list-item-content>
+                                                <v-list-item-title class="white--text">
+                                                        +1.703.934.3000
+                                                    </v-list-item-title>
+                                                </v-list-item-content>
+                                            </v-list-item>
+                                            
+                                            <v-list-item>
+                                                <v-list-item-icon>
+                                                    <v-icon color="white">
+                                                        mdi-fax
+                                                    </v-icon>
+                                                </v-list-item-icon>
+                                                <v-list-item-content>
+                                                <v-list-item-title class="white--text">
+                                                        +1.703.934.3740
+                                                    </v-list-item-title>
+                                                </v-list-item-content>
+                                            </v-list-item>
+
+                                            <v-list-item>
+                                                <v-list-item-icon>
+                                                    <v-icon color="white">
+                                                        mdi-map-marker
+                                                    </v-icon>
+                                                </v-list-item-icon>
+                                                <v-list-item-content>
+                                                <v-list-item-title class="white--text">
+                                                        9300 Lee Highway <br> Fairfax, VA 22031 US
+                                                    </v-list-item-title>
+                                                </v-list-item-content>
+                                            </v-list-item>
+                                        </v-list>
+                                    </v-col>
+                                </v-row>
+                            </v-card>
+                        </v-col>
+                    <v-spacer></v-spacer>
+                    <v-col>
+                        <v-card flat color="transparent" class="white--text" min-width="400px">
+                            <v-row>
+                                <v-col>
+                                    <div class="overline grey--text">
+                                        SOCIAL
+                                    </div>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col>
+                                    <v-btn
+                                        v-for="icon in icons"
+                                        :key="icon"
+                                        class="mx-4 white--text"
+                                        icon
+                                    >
+                                    <v-icon size="24px">
+                                        {{ icon }}
+                                    </v-icon>
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            icons: [
+                'mdi-facebook',
+                'mdi-twitter',
+                'mdi-linkedin',
+                'mdi-instagram',
+            ],
+        }
+    }
+}
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app-bar fixed prominent flat id="nav-bar" color="transparent">
+    <v-toolbar-title id="toolbar-title">
+      <v-img
+        contain
+        lazy-src="/images/logo.png"
+        max-height="85"
+        max-width="120"
+        src="/images/logo.png"
+      ></v-img>
+    </v-toolbar-title>
+    <v-spacer />
+    <v-row id="toolbar" justify="end" align="center">
+      <v-col cols="12" sm="10" md="10" class="py-0 my-0">
+        <v-text-field
+          label="SEARCH"
+          dense
+          solo
+          flat
+          outlined
+          append-icon="mdi-magnify"
+        >
+          <template v-slot:prepend>
+            <v-btn text>
+              <v-icon left>
+                mdi-menu-right
+              </v-icon>
+              <span class="hidden-sm-and-down">Blog</span>
+            </v-btn>
+
+            <v-btn text>
+              <v-icon left>
+                mdi-menu-right
+              </v-icon>
+              <span class="hidden-sm-and-down">News & Events</span>
+            </v-btn>
+
+            <v-btn text>
+              <v-icon left>
+                mdi-menu-right
+              </v-icon>
+              <span class="hidden-sm-and-down">Careers</span>
+            </v-btn>
+
+            <v-btn text>
+              <v-icon left>
+                mdi-menu-right
+              </v-icon>
+              <span class="hidden-sm-and-down">Contact</span>
+            </v-btn>
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <template v-slot:extension>
+      <v-tabs centered fixed-tabs>
+        <v-tab>Company</v-tab>
+        <v-tab>What We Do</v-tab>
+        <v-tab>Investors</v-tab>
+        <v-tab>Resources</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
+</template>
+
+<style>
+#nav-bar {
+  padding: 50px 150px 0px 150px;
+}
+#toolbar {
+  padding: 5px 5px 0px 5px;
+  margin: 5px 5px 0px 5px;
+}
+#toolbar-title {
+  background-color: white;
+  padding: 100px 5px 10px 5px;
+}
+</style>
